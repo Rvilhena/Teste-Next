6f640762200e6357b2113f7bc1e60f32c07a7ab6
Rvilhena <ricardocvilhena@gmail.com>
2021-04-07 19:03:05 -0300

    Including Link

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,16 @@
-import { useState} from 'react';
-
+import Link from 'next/link'
 
 function Home () {
     return (
         <div>
-            <h4>Home Teste do Next.Js e Deploy na Versel Ricardo 2</h4>
-            <Contador />
+            <h4>Home </h4>
+
+            <Link href="/sobre" >
+                <a>Acessar pagina Sobre</a>
+            </Link>
+
         </div>
     )
 }
-
-function Contador() {
-    const [contador, setContador] = useState(1);
-
-    function adicionarContador() {
-        setContador(contador + 1);
-    }
-
-    return (
-        <div>
-            <div>{contador}</div>
-            <button onClick={adicionarContador}>Adiciona</button>
-        </div>
-    )
-
-}
-
 
 export default Home
---
+++ pages/sobre.js
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+
+
+function Sobre () {
+    return (
+        <div>
+            <h4>Sobre </h4>
+            <Link href="/">
+                <a>Acessar pagina Home</a>
+            </Link>
+        </div>
+    )
+}
+
+export default Sobre
